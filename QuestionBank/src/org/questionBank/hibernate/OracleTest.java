3af730515acaf97aa0e9800b5de93fd32e88b497
Christian <brujosol@gmail.com>
2016-10-09 14:51:59 -0500

    Adding a file to test oracle connection

--- QuestionBank/src/org/questionBank/hibernate/OracleTest.java
+++ QuestionBank/src/org/questionBank/hibernate/OracleTest.java
@@ -1,8 +1,12 @@
 package org.questionBank.hibernate;
 
+import java.math.BigDecimal;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.questionBank.dao.CourseDataUtil;
+import org.questionBank.dao.InvalidCourseException;
 
 public class OracleTest {
 
@@ -10,14 +14,29 @@ public class OracleTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		UserDetails usr = new UserDetails();
-		usr.setUserId(2);
-		usr.setUserName("Testy");
-		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.save(usr);
-		session.getTransaction().commit();
+		CourseDataUtil courseDAO = new CourseDataUtil();
+		String courseNumber = "CSCI-6363";
+		String courseName = "Agile Software Engineering";
+		String title = "Agile";
+		String deptName = "Computer Science";
+		BigDecimal credit = new BigDecimal(3);
+		try{
+			courseDAO.createCourse(courseNumber, courseName, title, deptName, credit);
+		}catch(InvalidCourseException ex){
+			System.out.println("Invalid Course: "+ex.getMessage());
+			System.out.println(ex.getStackTrace());
+		}catch(Exception ex){
+			System.out.println("Reached Exception: " + ex.getMessage());
+			System.out.println(ex.getStackTrace());
+		}
+//		UserDetails usr = new UserDetails();
+//		usr.setUserId(2);
+//		usr.setUserName("Testy");
+//		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+//		Session session = sessionFactory.openSession();
+//		session.beginTransaction();
+//		session.save(usr);
+//		session.getTransaction().commit();
 	}
 
 }
