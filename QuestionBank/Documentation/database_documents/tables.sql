24ff486eb4a52681a3b5dfb76ea5fe10826ec3bd
Sefak Kahriman <sefak.kahriman@gmail.com>
2016-10-17 23:15:14 -0500

    Tables.sql changed

--- QuestionBank/Documentation/database_documents/tables.sql
+++ QuestionBank/Documentation/database_documents/tables.sql
@@ -5,43 +5,42 @@ drop table Person;
 drop table Course;
 
 CREATE TABLE Person(
-  per_id varchar(7),
+  per_id MEDIUMINT NOT NULL AUTO_INCREMENT,
   first_name varchar(50),
   last_name varchar(50),
-  user_name varchar(7),
+  user_name varchar(20),
   password varchar(20),
   primary key (per_id)
 );
 
 create table Course(
-  id              MEDIUMINT NOT NULL AUTO_INCREMENT,
-  course_number   varchar(10),
-  course_name     varchar(64),
-  title           varchar(64),
-  dept_name       varchar(32),
+  course_id   MEDIUMINT NOT NULL AUTO_INCREMENT,
+  course_name     varchar(50),
+  title           varchar(50),
+  dept_name       varchar(20),
   credit          int,
-  primary key (id)
+  primary key (course_id)
 );
 
-create table Teaches
-(per_id varchar(7),
+create table Teaches(
+per_id MEDIUMINT,
 course_id MEDIUMINT,
 primary key (per_id, course_id),
 foreign key (per_id) references Person(per_id) on delete cascade,
-foreign key (course_id) references Course(id) on delete cascade
+foreign key (course_id) references Course(course_id) on delete cascade
 );
 
-create table Question
-(question_id varchar(7),
+create table Question(
+question_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 course_id MEDIUMINT,
 chapter varchar(7),
 primary key (question_id),
-foreign key (course_id) references Course(id)
+foreign key (course_id) references Course(course_id)
 );
 
 create table Answer
-(answer_id varchar(7),
-question_id varchar(7),
+(answer_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+question_id MEDIUMINT,
 answer_text varchar(200),
 primary key (answer_id),
 foreign key (question_id) references Question(question_id)
